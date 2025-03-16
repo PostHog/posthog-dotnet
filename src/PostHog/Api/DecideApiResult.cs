f60c9530e20ce77b556dfea0c08c6c68cf56bbd5
Phil Haack <haacked@gmail.com>
2025-03-15 17:48:11 -0700

    Simplify the `DecideApiResult`
    These are the only properties we're concerned about now.

--- src/PostHog/Api/DecideApiResult.cs
+++ src/PostHog/Api/DecideApiResult.cs
@@ -1,21 +1,17 @@
-using System.Text.Json.Serialization;
 using PostHog.Json;
 
 namespace PostHog.Api;
 
 /// <summary>
-/// The API Result for the <c>/decide</c> endpoint. Some properties are ommitted because
+/// The API Result for the <c>/decide</c> endpoint. Some properties are omitted because
 /// they are not necessary for server-side scenarios.
 /// </summary>
+/// <param name="FeatureFlags">The feature flags that are enabled for the user.</param>
+/// <param name="ErrorsWhileComputingFlags">Whether there were errors while computing the feature flags.</param>
+/// <param name="FeatureFlagPayloads">The payloads of the feature flags.</param>
+/// <param name="QuotaLimited">The list of feature flags that are limited by quota.</param>
 internal record DecideApiResult(
-    FeatureFlagsConfig? Config = null,
-    bool IsAuthenticated = false,
     IReadOnlyDictionary<string, StringOrValue<bool>>? FeatureFlags = null,
-    Analytics? Analytics = null,
-    bool DefaultIdentifiedOnly = true,
     bool ErrorsWhileComputingFlags = false,
     IReadOnlyDictionary<string, string>? FeatureFlagPayloads = null,
     IReadOnlyList<string>? QuotaLimited = null);
-
-internal record FeatureFlagsConfig([property: JsonPropertyName("enable_collect_everything")] bool EnableCollectEverything);
-internal record Analytics(string Endpoint);
