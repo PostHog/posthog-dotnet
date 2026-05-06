d7b8c1d5186ebd37d058cdb1ed6c129eeae15555
Dustin Byrne <dustin@posthog.com>
2026-05-06 15:23:31 -0400

    style: follow PostHog property visibility convention

--- src/PostHog/Api/PostHogProperties.cs
+++ src/PostHog/Api/PostHogProperties.cs
@@ -6,8 +6,8 @@ public static class PostHogProperties
     public const string Lib = "$lib";
     public const string LibVersion = "$lib_version";
     public const string GeoIpDisable = "$geoip_disable";
-    internal const string ProcessPersonProfile = "$process_person_profile";
-    internal const string SessionId = "$session_id";
+    public const string ProcessPersonProfile = "$process_person_profile";
+    public const string SessionId = "$session_id";
     public const string Os = "$os";
     public const string Framework = "$framework";
     public const string Architecture = "$arch";
